3a64fe323038885a75926132c25eaa3c769feb10
Pol Dellaiera <pol.dellaiera@protonmail.com>
2026-05-05 19:30:21 +0200

    chore: release 0.1.3

--- nix/pkgs/cacache-ttl/package.nix
+++ nix/pkgs/cacache-ttl/package.nix
@@ -5,7 +5,7 @@
 
 rustPlatform.buildRustPackage {
   pname = "cacache-ttl";
-  version = "0.1.2";
+  version = "0.1.3";
 
   __structuredAttrs = true;
 
@@ -18,5 +18,5 @@ rustPlatform.buildRustPackage {
     ];
   };
 
-  cargoHash = "sha256-LLFIqT4+4FvqqMSlfwYmlkrUSM7WF/Kg1VFtX+9BDiI=";
+  cargoHash = "sha256-mZr/SxSOM218DML2bktcHriaAFok44Dye2MGWPUTddE=";
 }
